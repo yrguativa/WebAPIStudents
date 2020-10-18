d867e6dfb471e71f5d1d66f4042492c92402eac4
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2020-10-18 17:28:40 -0500

    Create only project with ef and initial migration

---
+++ StudentsAPI.Data/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentsAPI.Data
+{
+     public class DataBaseContext: DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ StudentsAPI.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StudentsAPI.Models
+{
+    public class Class1
+    {
+    }
+}
